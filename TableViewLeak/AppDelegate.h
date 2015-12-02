2f7a9e78d6034d92b7b24dbcbab4ea2965e3aeb4
Paul Derbyshire <paul.derbyshire@conkergroup.com>
2015-12-02 20:46:05 +0000

    Initial Commit

---
+++ TableViewLeak/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewLeak
+//
+//  Created by Paul Derbyshire on 02/12/2015.
+//  Copyright © 2015 iOSDigital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewLeak/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewLeak
+//
+//  Created by Paul Derbyshire on 02/12/2015.
+//  Copyright © 2015 iOSDigital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
